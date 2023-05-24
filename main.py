c857882d287b72b534d53afc9e2e6d13bfea7cca
Jose Carlos <joseca20025@gmail.com>
2023-05-23 20:54:02 -0400

    Agregada funcionalidad al boton

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import customtkinter as ctk
-import tkinter as tk
+import pyodbc
+from tabulate import tabulate
 from PIL import Image
 
 
@@ -9,6 +10,7 @@ class App(ctk.CTk):
         ctk.set_appearance_mode('system')
         ctk.set_default_color_theme('blue')
         self.geometry('500x700')
+        self.conexion = None
         self.iconbitmap('./image/icono.ico')
 
         self.loginPage = ctk.CTkFrame(self, width=500, height=700)
@@ -36,10 +38,37 @@ class App(ctk.CTk):
             self.userFrame, width=340, height=30, placeholder_text='Ingrese su contraseña')
         self.password.place(x=30, y=125)
 
-        self.boton = ctk.CTkButton(self.userFrame, text='Login')
+        self.boton = ctk.CTkButton(
+            self.userFrame, text='Login', command=self.login)
         self.boton.place(x=130, y=180)
 
         self.userFrame.place(x=50, y=430)
+
+    def login(self):
+        user = self.username.get()
+        password = self.password.get()
+        if user and password:
+            try:
+                self.conexion = pyodbc.connect(
+                    f'DRIVER={{SQL Server}};SERVER=JoseK-Laptop\SQLEXPRESS;DATABASE=FinalVeterinaria;UID={user};PWD={password}')
+
+                cursor = self.conexion.cursor()
+
+                cursor.execute('select * from Clientes')
+                resultado = cursor.fetchall()
+
+                nombreColumnas = [columna[0] for columna in cursor.description]
+
+                tabla = tabulate(resultado, headers=nombreColumnas,
+                                 tablefmt='fancy_grid')
+
+                print(tabla)
+
+                cursor.close()
+                conexion.close()
+
+            except:
+                pass
 
 
 app = App()
