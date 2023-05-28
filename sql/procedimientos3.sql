be0b27ea03fc5d4fd98ec9471b4336a15a6d4107
Mateo Fernandez <mateo.fersal1203@gmail.com>
2023-05-27 23:05:40 -0400

    Condicion if adicionada en el checkout

--- sql/procedimientos3.sql
+++ sql/procedimientos3.sql
@@ -15,57 +15,64 @@ create procedure CheckOutHuesped
 @NroHab char (2)
 AS
 BEGIN
-    declare CursorConsumos cursor for select CheckIn,CodMascota,TipoServ,Cantidad,Cargo from ConsumosEstadia
-    update Estadias set CheckOut=@CheckOut                                  --se coloca la fecha
-    where CheckIn=@CheckIn and CodMascota=@CodMascota and NroHab=@NroHab
-    update Habitaciones set Disponible = 'D' where NroHab=@NroHab           --la habitación esta disponible
-    DECLARE
-    @CheckAux date,
-    @CodAux char (5),
-    @TipoServAux varchar (20),
-    @Cantidad int,
-    @Cargo money,
-    @Sumador money = 0,
-    @Alias varchar (20) = (select Alias from Mascotas where CodMascota = @CodMascota),
-    @PrecioHotel money = (select Dias from Estadias where CheckIn=@CheckIn and CodMascota=@CodMascota and NroHab=@NroHab) * 60,
-    @Dias char (2) = (select Dias from Estadias where CheckIn=@CheckIn and CodMascota=@CodMascota and NroHab=@NroHab)
-    print 'La mascota '+@Alias+ ' a fecha de hoy, '+CONVERT(varchar (10), @CheckOut)+ ' realiza su Check Out de nuestro hotel.' 
-    print ''
-    print 'Por la estadía de '+@Dias+ 'dias en el hotel se genera un total de: Bs. '+convert(varchar(10),@PrecioHotel)
-    print ''
-    if Exists (select 1 from Requerimientos where CheckIn = @CheckIn and CodMascota=@CodMascota and NroHab=  @NroHab)
+    DECLARE @ver varchar(10)
+    set @ver = (select CheckOut from Estadias where CheckIn=@CheckIn and CodMascota=@CodMascota and NroHab=@NroHab)
+    if @ver is NULL
         BEGIN
-        print 'Ademas, Habiendo solicitado los siguientes servicios';
+        declare CursorConsumos cursor for select CheckIn,CodMascota,TipoServ,Cantidad,Cargo from ConsumosEstadia
+        update Estadias set CheckOut=@CheckOut                                  --se coloca la fecha
+        where CheckIn=@CheckIn and CodMascota=@CodMascota and NroHab=@NroHab
+        update Habitaciones set Disponible = 'D' where NroHab=@NroHab           --la habitación esta disponible
+        DECLARE
+        @CheckAux date,
+        @CodAux char (5),
+        @TipoServAux varchar (20),
+        @Cantidad int,
+        @Cargo money,
+        @Sumador money = 0,
+        @Alias varchar (20) = (select Alias from Mascotas where CodMascota = @CodMascota),
+        @PrecioHotel money = (select Dias from Estadias where CheckIn=@CheckIn and CodMascota=@CodMascota and NroHab=@NroHab) * 60,
+        @Dias char (2) = (select Dias from Estadias where CheckIn=@CheckIn and CodMascota=@CodMascota and NroHab=@NroHab)
+        print 'La mascota '+@Alias+ ' a fecha de hoy, '+CONVERT(varchar (10), @CheckOut)+ ' realiza su Check Out de nuestro hotel.' 
         print ''
-        print '―――――――――――――――――――――――――――――――――――――――――――――――――'
-        print '|  SERVICIO     -     CANTIDAD     -     CARGO  |'
-        print '―――――――――――――――――――――――――――――――――――――――――――――――――'
+        print 'Por la estadía de '+@Dias+ 'dias en el hotel se genera un total de: Bs. '+convert(varchar(10),@PrecioHotel)
         print ''
-        open CursorConsumos
-        fetch next from CursorConsumos into @CheckAux,@CodAux,@TipoServAux,@Cantidad,@Cargo
-        while(@@FETCH_STATUS=0)
+        if Exists (select 1 from Requerimientos where CheckIn = @CheckIn and CodMascota=@CodMascota and NroHab=  @NroHab)
             BEGIN
-            if (@CheckAux = @CheckIn and @CodAux = @CodMascota)
-                BEGIN
-                if @Cantidad is NULL
-                    SET @Cantidad = 1
-                PRINT '  ' + @TipoServAux + REPLICATE(' ', 22 - LEN(@TipoServAux))
-                        + ' ' + CONVERT(VARCHAR(18), @Cantidad) + REPLICATE(' ', 15 - LEN(CONVERT(VARCHAR(20), @Cantidad)))
-                        + ' ' + CONVERT(VARCHAR(10), @Cargo) + REPLICATE(' ', 10 - LEN(CONVERT(VARCHAR(10), @Cargo)));
-                set @Sumador += @Cargo
-                END
+            print 'Ademas, Habiendo solicitado los siguientes servicios';
+            print ''
+            print '―――――――――――――――――――――――――――――――――――――――――――――――――'
+            print '|  SERVICIO     -     CANTIDAD     -     CARGO  |'
+            print '―――――――――――――――――――――――――――――――――――――――――――――――――'
+            print ''
+            open CursorConsumos
             fetch next from CursorConsumos into @CheckAux,@CodAux,@TipoServAux,@Cantidad,@Cargo
-            END 
-        close CursorConsumos    
-        DEALLOCATE CursorConsumos
-        END
-    ELSE
-        print'No habiendo solicitado ningún servicio adicional'
+            while(@@FETCH_STATUS=0)
+                BEGIN
+                if (@CheckAux = @CheckIn and @CodAux = @CodMascota)
+                    BEGIN
+                    if @Cantidad is NULL
+                        SET @Cantidad = 1
+                    PRINT '  ' + @TipoServAux + REPLICATE(' ', 22 - LEN(@TipoServAux))
+                            + ' ' + CONVERT(VARCHAR(18), @Cantidad) + REPLICATE(' ', 15 - LEN(CONVERT(VARCHAR(20), @Cantidad)))
+                            + ' ' + CONVERT(VARCHAR(10), @Cargo) + REPLICATE(' ', 10 - LEN(CONVERT(VARCHAR(10), @Cargo)));
+                    set @Sumador += @Cargo
+                    END
+                fetch next from CursorConsumos into @CheckAux,@CodAux,@TipoServAux,@Cantidad,@Cargo
+                END 
+            close CursorConsumos    
+            DEALLOCATE CursorConsumos
+            END
+        ELSE
+            print'No habiendo solicitado ningún servicio adicional'
+            print''
+        print' '
+        print'Generándose un total de Bs. '+convert(varchar(10),(@PrecioHotel+@Sumador))+' por la estadía en el hotel'
         print''
-    print' '
-    print'Generándose un total de Bs. '+convert(varchar(10),(@PrecioHotel+@Sumador))+' por la estadía en el hotel'
-    print''
-    print'Atte. Hotel Happy Fox'
+        print'Atte. Hotel Happy Fox'
+        END
+    ELSE   
+        print 'Ya se ha realizado este Checkout'
 END
 
 select * from Servicios
@@ -90,3 +97,5 @@ select * from Estadias
 select * from Habitaciones -- solo la 1 estaria ocupada
 
 exec CheckOutHuesped '2023-05-26','2023-05-27','M0029','01'
+
+select * from Estadias
