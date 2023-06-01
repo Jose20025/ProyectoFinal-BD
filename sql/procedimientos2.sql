4ce6711f5e7e53120e87e11c207a692aedf10493
Mateo Fernandez <mateo.fersal1203@gmail.com>
2023-06-01 16:26:33 -0400

    cambios procedimientos2

--- sql/procedimientos2.sql
+++ sql/procedimientos2.sql
@@ -2,10 +2,23 @@
 create procedure RegistrarPeso
 @FechaPeso date,
 @CodMascota char(5),
-@Peso float
+@Peso float,
+@Check bit out
 as 
 BEGIN
-    insert into HistorialesPeso values (@FechaPeso,@CodMascota,@Peso)
+    begin TRANSACTION
+    if exists (select 1 from HistorialesPeso 
+                where FechaPeso=@FechaPeso and CodMascota=@CodMascota)
+        BEGIN
+        set @Check = 0
+        SELECT @Check
+        end
+    else
+        BEGIN
+        set @Check = 1
+        insert into HistorialesPeso values (@FechaPeso,@CodMascota,@Peso)
+        insert into HistorialesPeso values (@FechaPeso,@CodMascota,@Peso)
+        END
 END
 GO;
 
@@ -13,57 +26,130 @@ GO;
 create procedure ModificarPeso
 @FechaPeso date,
 @CodMascota char (5),
-@NuevoPeso float
+@NuevoPeso float,
+@Check bit out
 AS
 BEGIN
+    begin TRANSACTION
+    DECLARE @SQL NVARCHAR(MAX);
     update HistorialesPeso set Peso = @NuevoPeso 
     where CodMascota = @CodMascota and FechaPeso = @FechaPeso  
+    if exists (select 1 from HistorialesPeso 
+        where FechaPeso=@FechaPeso and CodMascota=@CodMascota and Peso=@NuevoPeso)
+        BEGIN
+        set @Check = 1
+        SELECT @Check
+        END 
+    else    
+        BEGIN
+        set @Check = 0
+        SELECT @Check
+        END
 END
 GO;
 
 --Eliminar un registro de peso
 create procedure EliminarRegPeso
 @FechaPeso date,
-@CodMascota char (5)
+@CodMascota char (5),
+@Cuenta int,
+@Check bit out
 AS
-BEGIN
+BEGIN 
+    begin TRANSACTION
     delete from HistorialesPeso 
-    where CodMascota = @CodMascota and FechaPeso = @FechaPeso 
+    where CodMascota = @CodMascota and FechaPeso = @FechaPeso
+    select @Cuenta = count(*) from HistorialesPeso
+        where FechaPeso=@FechaPeso and CodMascota=@CodMascota
+    if @Cuenta = 0
+        BEGIN
+        set @Check  = 1
+        SELECT @Check
+        END
+    else 
+        BEGIN
+        set @Check = 0
+        SELECT @Check
+        END
 END 
 GO;
 
---procedimiento de registro de visita medica
+--procedimiento de registro de visita medica 
 create PROCEDURE RegistrarVisitaMedica 
 @FechaConsulta date,
 @CodMascota char(5),
 @Situacion varchar(20),
-@DetalleMed varchar(40)
+@DetalleMed varchar(40),
+@Check bit out
 as 
 BEGIN
-    insert into HistorialesMedicos values (@FechaConsulta,@CodMascota,@Situacion,@DetalleMed)
+    begin TRANSACTION
+    if exists (select 1 from HistorialesMedicos where FechaConsulta=@FechaConsulta and CodMascota=@CodMascota)
+        BEGIN
+        set @Check = 0
+        SELECT @Check
+        end
+    else
+        BEGIN
+        insert into HistorialesMedicos values (@FechaConsulta,@CodMascota,@Situacion,@DetalleMed)
+        set @Check = 1
+        SELECT @Check
+        END
 END
 GO;
 
 --Modificar una visita medica
-create procedure ModificarVisitaMed
-@FechaConsulta date,
-@CodMascota char(5),
-@Campo varchar (20),
-@NuevoValor varchar (40)
+alter PROCEDURE ModificarVisitaMed
+    @FechaConsulta DATE,
+    @CodMascota CHAR(5),
+    @Campo VARCHAR(20),
+    @NuevoValor VARCHAR(40),
+    @Check BIT OUT
 AS
 BEGIN
-    update HistorialesMedicos set @Campo = @NuevoValor
-    where FechaConsulta = @FechaConsulta and CodMascota = @CodMascota
+    BEGIN TRANSACTION;
+
+    DECLARE @SQL NVARCHAR(MAX);
+
+    SET @SQL = N'UPDATE HistorialesMedicos SET ' + QUOTENAME(@Campo) + ' = @NuevoValor WHERE FechaConsulta = @FechaConsulta AND CodMascota = @CodMascota';
+
+    EXEC sp_executesql @SQL, N'@FechaConsulta DATE, @CodMascota CHAR(5), @NuevoValor VARCHAR(40)', @FechaConsulta, @CodMascota, @NuevoValor;
+
+    IF EXISTS (SELECT 1 FROM HistorialesMedicos WHERE FechaConsulta = @FechaConsulta AND CodMascota = @CodMascota AND ' + QUOTENAME(@Campo) + ' = @NuevoValor)
+    BEGIN
+        SET @Check = 1;
+        SELECT @Check;
+    END
+    ELSE
+    BEGIN
+        SET @Check = 0;
+        SELECT @Check;
+    END
 END
+
 GO;
 
 --Eliminar una visita medica
 create procedure EliminarVisitaMed
 @FechaConsulta date,
-@CodMascota char(5)
+@CodMascota char(5),
+@Cuenta int,
+@Check bit out
 AS
 BEGIN
+    begin TRANSACTION
     delete from HistorialesMedicos where FechaConsulta = @FechaConsulta and CodMascota = @CodMascota
+    select @Cuenta = count (*) from HistorialesMedicos where FechaConsulta = @FechaConsulta and CodMascota = @CodMascota
+    if @Cuenta = 0
+        BEGIN
+        set @Check = 1
+        SELECT @Check
+        end
+    else    
+        BEGIN
+        set @Check = 0
+        SELECT @Check
+        end
 END
 GO; 
 
@@ -72,10 +158,22 @@ create PROCEDURE RegistrarVacunacion
 @FechaVacuna date,
 @CodMascota char (5),
 @TipoVacuna varchar(15),
-@Fabricante varchar (20)
+@Fabricante varchar (20),
+@Check bit out
 as 
 BEGIN
-    insert into CalendariosVacunas values (@FechaVacuna,@CodMascota,@TipoVacuna,@Fabricante)
+    begin TRANSACTION 
+    if exists (select 1 from CalendariosVacunas 
+                where FechaVacuna=@FechaVacuna and CodMascota=@CodMascota and TipoVacuna=@TipoVacuna and Fabricante=@Fabricante)
+        BEGIN
+        set @Check = 0
+        SELECT @Check
+        end
+    ELSE
+        BEGIN
+        insert into CalendariosVacunas values (@FechaVacuna,@CodMascota,@TipoVacuna,@Fabricante)    
+        set @Check = 1
+        END
 END
 GO;
 
@@ -87,11 +185,26 @@ create procedure EliminarVacunacion
 @FechaVacuna date,
 @CodMascota char (5),
 @TipoVacuna varchar(15),
-@Fabricante varchar (20)
+@Fabricante varchar (20),
+@Cuenta int,
+@Check bit out
 AS
 BEGIN
+    begin TRANSACTION
     delete from CalendariosVacunas
-    where FechaVacuna=@FechaVacuna and CodMascota=@CodMascota and TipoVacuna=@TipoVacuna and Fabricante=@Fabricante
+        where FechaVacuna=@FechaVacuna and CodMascota=@CodMascota and TipoVacuna=@TipoVacuna and Fabricante=@Fabricante
+    select @Cuenta = count(*) from CalendariosVacunas 
+        where FechaVacuna=@FechaVacuna and CodMascota=@CodMascota and TipoVacuna=@TipoVacuna and Fabricante=@Fabricante
+    if @Cuenta = 0
+        BEGIN
+        set @Check = 1
+        SELECT @Check
+        end
+    else   
+        begin
+        set @Check = 0
+        SELECT @Check
+        end
 END
 GO;
 
@@ -100,45 +213,102 @@ create procedure RegistrarEstadia
 @CheckIn date,
 @CodMascota char (5),
 @NroHab char (2),
-@Dias int
+@Dias int,
+@Check bit out
 AS
-BEGIN
-    insert into Estadias values (@CheckIn,@CodMascota,@NroHab,NULL,@Dias)
-    update Habitaciones set Disponible = 'O' where NroHab = @NroHab
+BEGIN  
+    begin TRANSACTION
+    if exists(select 1 from Estadias where CheckIn=@CheckIn and CodMascota=@CodMascota and NroHab=@NroHab)
+        begin
+        set @Check = 0
+        SELECT @Check
+        end
+    else    
+        BEGIN
+        insert into Estadias values (@CheckIn,@CodMascota,@NroHab,NULL,@Dias)
+        update Habitaciones set Disponible = 'O' where NroHab = @NroHab 
+        set @Check = 1
+        SELECT @Check
+        END
 END
 GO;
 
 --Modificación de una estadía
-create procedure ModificarEstadia
-@CheckIn date,
-@CodMascota char (5),
-@NroHab char (2),
-@Campo varchar (20),
-@NuevoValor varchar (20)
-as
+alter PROCEDURE ModificarEstadia
+    @CheckIn DATE,
+    @CodMascota CHAR(5),
+    @NroHab CHAR(2),
+    @Campo VARCHAR(20),
+    @NuevoValor VARCHAR(20),
+    @Check BIT OUT
+AS
 BEGIN
-    if @Campo = 'NroHab'
+    BEGIN TRANSACTION;
+
+    IF @Campo = 'NroHab'
+    BEGIN
+        UPDATE Habitaciones SET Disponible = 'D' WHERE NroHab = @NroHab;
+        UPDATE Estadias SET NroHab = @NuevoValor WHERE CheckIn = @CheckIn AND CodMascota = @CodMascota AND NroHab = @NroHab;
+        UPDATE Habitaciones SET Disponible = 'O' WHERE NroHab = @NuevoValor;
+
+        IF EXISTS (SELECT 1 FROM Estadias WHERE CheckIn = @CheckIn AND CodMascota = @CodMascota AND NroHab = @NuevoValor)
         BEGIN
-        update Habitaciones set Disponible = 'D' where NroHab = @NroHab
-        update Estadias set NroHab = @NuevoValor 
-        where CheckIn = @CheckIn and CodMascota=@CodMascota and NroHab=@NroHab 
-        update Habitaciones set Disponible = 'O' where NroHab=@NuevoValor
-        END 
-    else 
-        update Estadias set @Campo = @NuevoValor
-        where CheckIn = @CheckIn and CodMascota=@CodMascota and NroHab=@NroHab
+            SET @Check = 1;
+            SELECT @Check;
+        END
+        ELSE
+        BEGIN
+            SET @Check = 0;
+            SELECT @Check;
+        END;
+    END
+    ELSE
+    BEGIN
+        DECLARE @SQL NVARCHAR(MAX);
+
+        SET @SQL = N'UPDATE Estadias SET ' + QUOTENAME(@Campo) + ' = @NuevoValor WHERE CheckIn = @CheckIn AND CodMascota = @CodMascota AND NroHab = @NroHab';
+
+        EXEC sp_executesql @SQL, N'@CheckIn DATE, @CodMascota CHAR(5), @NuevoValor VARCHAR(20)', @CheckIn, @CodMascota, @NuevoValor;
+
+        IF EXISTS (SELECT 1 FROM Estadias WHERE CheckIn = @CheckIn AND CodMascota = @CodMascota AND NroHab = @NroHab AND ' + QUOTENAME(@Campo) + ' = @NuevoValor)
+        BEGIN
+            SET @Check = 1;
+            SELECT @Check;
+        END
+        ELSE
+        BEGIN
+            SET @Check = 0;
+            SELECT @Check;
+        END;
+    END;
 END
+
 GO;
 
 --Eliminar una estadía
 create procedure EliminarEstadia
 @CheckIn date,
 @CodMascota char (5),
-@NroHab char (2)
+@NroHab char (2),
+@Cuenta int,
+@Check bit out
 AS
 BEGIN
+    begin TRANSACTION
     delete from Estadias
     where CheckIn = @CheckIn and CodMascota=@CodMascota and NroHab=@NroHab
+    SELECT @Cuenta = count(*) from Estadias 
+        where CheckIn = @CheckIn and CodMascota=@CodMascota and NroHab=@NroHab
+    if @Cuenta = 0
+        BEGIN
+        set @Check = 1
+        SELECT @Check
+        end
+    else
+        begin
+        set @Check = 0
+        SELECT @Check
+        end
 END
 GO;
 
@@ -166,30 +336,27 @@ create procedure RegistrarRequerimiento
 @CheckIn date, 
 @Cantidad int,
 @NroHab char (2),
-@Dias int
+@Dias int,
+@Check bit out
 AS
 BEGIN 
-    DECLARE
-    @Tamaño char (2),
-    @Cargo money,
-	@Factor float,
-    @Precio money
-    --son servicios especiales y no tienen cantidad, se cobra por la cantidad de dias
-    if @IdServicio = 'S04' or @IdServicio = 'S05'
-        BEGIN
-        set @Cargo = (select Precio from Servicios where IdServicio = @IdServicio)
-        set @Cargo *= @Dias
-        insert into Requerimientos values (@IdServicio,@CheckIn,@CodMascota,@NroHab,NULL,@Cargo)
-        END
-    --servicios que pueden llegar a realizarse hasta una vez por estadía
-    if @IdServicio = 'S02'
-        BEGIN
-        set @Cargo = (select Precio from Servicios where IdServicio = @IdServicio)
-        insert into Requerimientos values (@IdServicio,@CheckIn,@CodMascota,@NroHab,1,@Cargo)
-        END
+    begin TRANSACTION
+    if exists (select 1 from Requerimientos 
+                where IdServicio=@IdServicio and CheckIn=@CheckIn and CodMascota=@CodMascota and NroHab=@NroHab)
+        begin
+        set @Check = 0
+        SELECT @Check
+        end
     else
     --los demás servicios que se pueden realizar varias veces y se cobran de acuerdo al tamaño, cantidad de veces
         BEGIN
+         DECLARE
+        @Tamaño char (2),
+        @Cargo money,
+        @Factor float,
+        @Precio money
+        set @Check = 1
+        SELECT @Check
         set @Tamaño = (select Tamaño from Mascotas where CodMascota = @CodMascota)
         set @Precio = (select Precio from Servicios where IdServicio = @IdServicio)
         if @Tamaño = 'S'
@@ -202,20 +369,52 @@ BEGIN
             set @Factor = 1.40
         set @Cargo = @Cantidad * @Precio * @Factor
         insert into Requerimientos values (@IdServicio,@CheckIn,@CodMascota,@NroHab,@Cantidad,@Cargo)
+       
+        --son servicios especiales y no tienen cantidad, se cobra por la cantidad de dias
+        if @IdServicio = 'S04' or @IdServicio = 'S05'
+            BEGIN
+            set @Cargo = (select Precio from Servicios where IdServicio = @IdServicio)
+            set @Cargo *= @Dias
+            insert into Requerimientos values (@IdServicio,@CheckIn,@CodMascota,@NroHab,NULL,@Cargo)
+            END
+        --servicios que pueden llegar a realizarse hasta una vez por estadía
+        if @IdServicio = 'S02'
+            BEGIN
+            set @Cargo = (select Precio from Servicios where IdServicio = @IdServicio)
+            insert into Requerimientos values (@IdServicio,@CheckIn,@CodMascota,@NroHab,1,@Cargo)
+            END
+        else
+        --los demás servicios que se pueden realizar varias veces y se cobran de acuerdo al tamaño, cantidad de veces
+            BEGIN
+            set @Tamaño = (select Tamaño from Mascotas where CodMascota = @CodMascota)
+            set @Precio = (select Precio from Servicios where IdServicio = @IdServicio)
+            if @Tamaño = 'S'
+                set @Factor = 1
+            if @Tamaño = 'M'
+                set @Factor = 1.15
+            if @Tamaño = 'G'
+                set @Factor = 1.30
+            if @Tamaño = 'XG'
+                set @Factor = 1.40
+            set @Cargo = @Cantidad * @Precio * @Factor
+            insert into Requerimientos values (@IdServicio,@CheckIn,@CodMascota,@NroHab,@Cantidad,@Cargo)
+            END
         END
 END  
 GO;
 
 --Modificacion de requerimiento
-create procedure ModificarRequerimiento
+alter procedure ModificarRequerimiento
 @IdServicio char (3),
 @CheckIn date,
 @CodMascota char (5),
 @NroHab char (2),
 @Campo varchar (10),
-@NuevoValor int
+@NuevoValor int,
+@Check bit out
 AS
 BEGIN
+    begin TRANSACTION
     if @IdServicio='S01' or @IdServicio='S03' or @IdServicio ='S06'
         if @Campo = 'Cantidad'
             BEGIN
@@ -238,6 +437,17 @@ BEGIN
                 update Requerimientos set Cantidad=@NuevoValor, Cargo=@Cargo
                 where IdServicio=@IdServicio and CheckIn=@CheckIn and CodMascota=@CodMascota and NroHab=@NroHab
             END
+    if exists (select 1 from Requerimientos
+        where IdServicio=@IdServicio and CheckIn=@CheckIn and CodMascota=@CodMascota and NroHab=@NroHab and Cantidad=@NuevoValor)
+        begin
+        set @Check = 1
+        SELECT @Check
+        end
+    else   
+        begin
+        set @Check = 0
+        SELECT @Check
+        end
 END
 GO;
 
@@ -246,11 +456,26 @@ create procedure EliminarRequerimiento
 @IdServicio char (3),
 @CheckIn date,
 @CodMascota char (5),
-@NroHab char (2)
+@NroHab char (2),
+@Cuenta int,
+@Check bit out 
 AS
 BEGIN
+    begin TRANSACTION
     delete from Requerimientos
     where IdServicio=@IdServicio and CheckIn=@CheckIn and CodMascota=@CodMascota and NroHab=@NroHab
+    select @Cuenta = count(*) from Requerimientos
+        where IdServicio=@IdServicio and CheckIn=@CheckIn and CodMascota=@CodMascota and NroHab=@NroHab
+    if @Cuenta = 0
+        begin
+        set @Check = 1
+        SELECT @Check
+        end
+    ELSE    
+        begin
+        set @Check = 0
+        SELECT @Check
+        end
 END
 GO;
 
@@ -258,58 +483,44 @@ GO;
 create procedure AgregarVacuna
 @TipoVacuna varchar(15),
 @Fabricante varchar(20),
-@Precio money
+@Precio money,
+@Check bit out
 AS
 BEGIN
-    insert into Vacunas values (@TipoVacuna,@Fabricante,@Precio)
+    begin TRANSACTION
+    if exists (select 1 from Vacunas where TipoVacuna=@TipoVacuna and Fabricante=@Fabricante)
+        begin
+        set @Check = 0
+        SELECT @Check
+        end
+    else
+        BEGIN
+        set @Check = 1
+        SELECT @Check
+        insert into Vacunas values (@TipoVacuna,@Fabricante,@Precio)
+        END
 END
 GO;
 
 --Eliminar vacuna
 create procedure EliminarVacuna
 @TipoVacuna varchar(15),
-@Fabricante varchar (20)
+@Fabricante varchar (20),
+@Cuenta int,
+@Check bit out
 AS
 BEGIN
+    begin TRANSACTION
     delete from Vacunas where TipoVacuna=@TipoVacuna and Fabricante=@Fabricante
+    select @Cuenta = count (*) from Vacunas where TipoVacuna=@TipoVacuna and Fabricante=@Fabricante
+    if @Cuenta = 0
+        begin
+        set @Check = 1
+        SELECT @Check
+        end
+    ELSE
+        begin
+        set @Check = 0
+        SELECT @Check
+        end
 END
-
-select * from Servicios
-
-select * from Servicios 
-insert into Mascotas values ('M0077','C0001','Test','Felino','Bombay','Negro','2020/01/15','G')
-insert into Estadias values ('2023-05-06','M0077','01',NULL,7)
-select * from Estadias
-exec RegistrarRequerimiento 'M0077','S03','2023-05-06',4,'01',7
-select * from Requerimientos
-
-
-
-
-
-
-
-
-    
-    
-    
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
