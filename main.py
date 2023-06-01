5cedb3de8297a1737b1d5180febb90ee3998d779
Jose Carlos <joseca20025@gmail.com>
2023-06-01 01:05:28 -0400

    Migrado a ttk

--- main.py
+++ main.py
@@ -1,89 +1,83 @@
-import customtkinter as ctk
-from tkinter import StringVar, ttk
-from CTkMessagebox import CTkMessagebox as msg
+from tkinter import ttk
+from tkinter.messagebox import showerror, showwarning, showinfo
+import tkinter as tk
 import pyodbc
-from PIL import Image
+from PIL import ImageTk, Image
 
 
-class App(ctk.CTk):
+class App(tk.Tk):
     def __init__(self):
-        global user, password, lugar
-
         super().__init__()
-        ctk.set_appearance_mode('system')
-        ctk.set_default_color_theme('blue')
+
+        # Aplicando el tema
+        self.tk.call('source', 'azure.tcl')
+        self.tk.call('set_theme', 'dark')
+
+        self.iconbitmap('./image/icono.ico')
         self.geometry('500x700')
         self.title('Login')
-        self.iconbitmap('./image/icono.ico')
         self.resizable(0, 0)
-        self.mainPageHotel = HotelPage(self)
-        self.mainPageVeterinaria = VeterinariaPage(self)
 
         self.loginPage = LoginPage(self)
         self.loginPage.pack()
 
-        self.centrarVentana(500, 700)
+        # Asignando variables de ventanas
+        self.veterinaria = None
+        self.hotel = None
 
-    def centrarVentana(self, ancho, alto):
-        self.update()
-        x = (self.winfo_screenwidth() // 2) - (ancho // 2)
-        y = (self.winfo_screenheight() // 2) - (alto // 2)
-        self.geometry(f'{ancho}x{alto}+{x}+{y-20}')
+        self.centrarVentana([500, 700])
 
-    def cambioVentana(self, old: ctk.CTkFrame, new: ctk.CTkFrame, ancho, alto, titulo):
-        old.destroy()
-        self.geometry(f'{ancho}x{alto}')
+    def cambioVentana(self, old, new, dimensiones: list, titulo):
         self.title(titulo)
-        self.centrarVentana(ancho, alto)
-        self.update()
+        old.pack_forget()
+        self.geometry(f'{dimensiones[0]}x{dimensiones[1]}')
+        self.centrarVentana(dimensiones)
         new.pack()
 
+    def centrarVentana(self, dimensiones: list):
+        x = (self.winfo_screenwidth() // 2) - (dimensiones[0] // 2)
+        y = (self.winfo_screenheight() // 2) - (dimensiones[1] // 2)
+        self.geometry(f'{dimensiones[0]}x{dimensiones[1]}+{x}+{y-20}')
 
-class LoginPage(ctk.CTkFrame):
+
+class LoginPage(ttk.Frame):
     def __init__(self, master: App = None):
-        super().__init__(master=master, width=500, height=700)
+        super().__init__(master, width=500, height=700)
 
         self.padre = master
-        self.lugar = None
 
-        self.logo = ctk.CTkLabel(self, image=ctk.CTkImage(light_image=Image.open(
-            './image/logo-transparente.png'), dark_image=Image.open('./image/logo-transparente.png'), size=(400, 370)), width=400, height=340, text='',
+        self.userFrame = ttk.Frame(
+            self, width=400, height=230, style='Card.TFrame')
 
-            corner_radius=5)
-        self.logo.place(x=50, y=40)
+        self.imagen = ImageTk.PhotoImage(Image.open(
+            './image/logo-transparente.png').resize((400, 370)))
 
-        # User Frame
-        self.userFrame = ctk.CTkFrame(self, width=400, height=230)
+        self.label = ttk.Label(self, image=self.imagen, justify=tk.CENTER)
+        self.label.place(x=50, y=40)
 
-        ctk.CTkLabel(
-            self.userFrame, text='Username').place(x=40, y=20)
+        ttk.Label(self.userFrame, text='Username').place(x=40, y=20)
 
-        self.username = ctk.CTkEntry(
-            self.userFrame, width=340, height=30, placeholder_text='Ingrese su usuario')
+        self.username = ttk.Entry(self.userFrame, width=46)
         self.username.place(x=30, y=45)
-        self.username.bind('<Return>', self.login)
 
-        ctk.CTkLabel(
-            self.userFrame, text='Password').place(x=40, y=100)
+        ttk.Label(self.userFrame, text='Password').place(x=40, y=100)
 
-        self.password = ctk.CTkEntry(
-            self.userFrame, width=340, height=30, placeholder_text='Ingrese su contraseña', show='*')
+        self.password = ttk.Entry(self.userFrame, width=46, show='*')
         self.password.place(x=30, y=125)
         self.password.bind('<Return>', self.login)
 
-        self.boton = ctk.CTkButton(
-            self.userFrame, text='Login', command=self.login)
-        self.boton.place(x=230, y=180)
+        self.loginBoton = ttk.Button(
+            self.userFrame, text='Login', style='Accent.TButton', command=self.login)
+        self.loginBoton.place(x=275, y=180)
 
-        self.combobox = ctk.CTkComboBox(
-            self.userFrame, values=['Veterinaria', 'Hotel'], state='readonly')
-        self.combobox.set('Veterinaria')
-        self.combobox.place(x=30, y=180)
+        self.lugarCBox = ttk.Combobox(self.userFrame, values=[
+                                      'Veterinaria', 'Hotel'], state='readonly')
+        self.lugarCBox.set('Veterinaria')
+        self.lugarCBox.place(x=30, y=180)
 
         self.userFrame.place(x=50, y=430)
 
     def login(self, event=None):
-
         self.user = self.username.get()
         self.passwd = self.password.get()
         if self.user and self.passwd:
@@ -92,153 +86,69 @@ class LoginPage(ctk.CTkFrame):
                     self.conexion = pyodbc.connect(
                         f'DRIVER={{SQL Server}};SERVER={conexiones[self.user][1]};DATABASE=FinalVeterinaria;UID={self.user};PWD={self.passwd}')
                     self.conexion.close()
-
                 except:
-                    msg(title='Error en la conexion',
-                        message='Usuario o contraseña incorrectos', icon='cancel')
-
+                    showerror(title='Error en la conexion',
+                              message='Ha ocurrido un error en la conexion a la base de datos')
                 else:
                     user.set(self.user)
                     password.set(self.passwd)
                     print(user.get())
-                    self.lugar = self.combobox.get()
-                    self.eleccion()
-
+                    self.eleccion(self.lugarCBox.get())
             else:
-                msg(title='Error en la conexion',
-                    message='Usuario o contraseña incorrectos', icon='cancel')
+                showerror(title='Error en la conexion',
+                          message='Usuario o contraseña incorrectos')
         elif not self.user and self.passwd:
-            msg(title='Usuario es requerido',
-                message='El campo de username no puede estar vacio', icon='cancel')
+            showerror(title='Usuario es requerido',
+                      message='El campo de username no puede estar vacio')
         elif not self.passwd and self.user:
-            msg(title='La contraseña es requerida',
-                message='El campo de contraseña no puede estar vacio', icon='cancel')
+            showerror(title='La contraseña es requerida',
+                      message='El campo de contraseña no puede estar vacio')
         else:
-            msg(title='Campos requeridos',
-                message='Los dos campos no pueden estar vacios', icon='cancel')
+            showerror(title='Campos requeridos',
+                      message='Los dos campos no pueden estar vacios')
 
-    def eleccion(self):
-        if self.lugar == 'Veterinaria':
+    def eleccion(self, lugar):
+        if lugar == 'Veterinaria':
+            self.padre.veterinaria = VeterinariaPage()
             self.padre.cambioVentana(
-                self, self.padre.mainPageVeterinaria, 1000, 600, 'Cute Pets - Veterinaria')
+                self, self.padre.veterinaria, [1000, 600], 'Cute Pets - Veterinaria')
         else:
+            self.padre.hotel = HotelPage()
             self.padre.cambioVentana(
-                self, self.padre.mainPageHotel, 1000, 600, 'Cute Pets - Hotel')
+                self, self.padre.hotel, [1000, 600], 'Cute Pets - Hotel')
 
 
-class HotelPage(ctk.CTkFrame):
-    def __init__(self, master=None):
-        super().__init__(master=master, width=1000, height=600)
-
-        self.padre = master
-        self.cursor = None
-
-        self.tablaFrame = Tabla(self, 640, 340)
-
-        ctk.CTkButton(self, text='Consultas', width=210,
-                      height=40).place(x=20, y=30)
-
-        ctk.CTkButton(self, text='Insertar', width=210,
-                      height=40).place(x=20, y=100)
-
-        ctk.CTkButton(self, text='Modificar', width=210,
-                      height=40).place(x=20, y=170)
-
-        ctk.CTkButton(self, text='Eliminar', width=210,
-                      height=40).place(x=20, y=240)
-
-        ctk.CTkButton(self, text='Consulta Personalizada', width=210,
-                      height=40).place(x=20, y=310)
-
-        self.imagen = ctk.CTkImage(dark_image=Image.open(
-            './image/logo_sinletras.jpg'), size=(210, 180))
-
-        ctk.CTkLabel(
-            self, width=190, height=160, image=self.imagen, text='').place(x=20, y=400)
+class HotelPage(ttk.Frame):
+    pass
 
 
-class VeterinariaPage(ctk.CTkFrame):
-    def __init__(self, master=None):
+class VeterinariaPage(ttk.Frame):
+    def __init__(self, master: App = None):
         super().__init__(master=master, width=1000, height=600)
 
         self.padre = master
 
-        self.tablaFrame = Tabla(self, 640, 340)
+        ttk.Button(self, text='Consultas', width=40).place(x=20, y=30)
 
-        ctk.CTkButton(self, text='Consultas', width=210,
-                      height=40).place(x=20, y=30)
+        ttk.Button(self, text='Insertar', width=40).place(x=20, y=100)
 
-        ctk.CTkButton(self, text='Insertar', width=210,
-                      height=40).place(x=20, y=100)
+        ttk.Button(self, text='Modificar', width=40).place(x=20, y=170)
 
-        ctk.CTkButton(self, text='Modificar', width=210,
-                      height=40).place(x=20, y=170)
+        ttk.Button(self, text='Eliminar', width=40).place(x=20, y=240)
 
-        ctk.CTkButton(self, text='Eliminar', width=210,
-                      height=40).place(x=20, y=240)
+        ttk.Button(self, text='Consulta Personalizada',
+                   width=40).place(x=20, y=310)
 
-        ctk.CTkButton(self, text='Consulta Personalizada', width=210,
-                      height=40).place(x=20, y=310)
+        self.imagen = ImageTk.PhotoImage(Image.open(
+            './image/logo-transparente.png').resize((220, 200)))
 
-        self.imagen = ctk.CTkImage(dark_image=Image.open(
-            './image/logo_sinletras.jpg'), size=(210, 180))
-
-        ctk.CTkLabel(
-            self, width=190, height=160, image=self.imagen, text='').place(x=20, y=400)
-
-
-class Tabla(ctk.CTkFrame):
-    def __init__(self, master, ancho, alto):
-        super().__init__(master, width=ancho, height=alto)
-
-    def setTabla(self, consulta):
-        print(user.get(), password.get())
-
-        self.user = user.get()
-        self.password = password.get()
-
-        self.conexion = pyodbc.connect(
-            f'DRIVER={{SQL Server}};SERVER={conexiones[self.user][1]};DATABASE=FinalVeterinaria;UID={self.user};PWD={self.password}')
-        self.cursor = self.conexion.cursor()
-
-        self.cursor.execute(consulta)
-
-        titulos = []
-
-        for titulo in self.cursor.description:
-            titulos.append(titulo[0])
-
-        mascotas = self.cursor.fetchall()
-
-        self.tabla = ttk.Treeview(
-            self, columns=titulos[1:], height=20)
-
-        for titulo in titulos:
-            if titulo == titulos[0]:
-                self.tabla.column('#0', width=100, anchor=ctk.CENTER)
-                self.tabla.heading('#0', text=titulo)
-            else:
-                self.tabla.column(titulo, width=100, anchor=ctk.CENTER)
-                self.tabla.heading(titulo, text=titulo)
-
-        for mascota in mascotas:
-            atributos = []
-            for atributo in mascota:
-                atributos.append(atributo)
-
-            self.tabla.insert(
-                '', ctk.END, text=atributos[0], values=atributos[1:])
-
-        self.tabla.pack()
-        self.conexion.close()
-
-        self.place(x=310, y=10)
+        ttk.Label(self, image=self.imagen).place(x=20, y=350)
 
 
 conexiones = {'josek': ['password', 'JoseK-Laptop\SQLEXPRESS'],
               'nangui': ['soychurro', 'BrunoPC']}
 
 app = App()
-user = StringVar()
-password = StringVar()
+user = tk.StringVar()
+password = tk.StringVar()
 app.mainloop()
