013bebb8f99d56d4bcb693f0599fa930df7b2410
Mateo Fernandez <mateo.fersal1203@gmail.com>
2023-06-01 16:12:46 -0400

    adicion de procedimientos 5

---
+++ sql/procedimientos5.sql
@@ -0,0 +1,40 @@
+alter procedure BuscarMascota
+@Campo varchar (10),
+@Valor varchar (15)
+AS  
+BEGIN
+    declare @SQL nvarchar(max)
+    
+    set @SQL = N'select CodMascota,Alias,Apellido,Especie,Color_pelo from Mascotas
+                inner join Clientes on Clientes.IdCliente = Mascotas.IdCliente
+                where' +QUOTENAME(@Campo)+'=@Valor' 
+
+    exec sp_executesql @SQL, N'@Valor varchar(15)',@Valor
+end
+exec BuscarMascota 'Alias','Tupac'
+
+select Alias from Mascotas
+
+declare @p bit; 
+set @p = 1
+begin TRANSACTION
+exec ModificarMascota 'M0001','Color_pelo','Verde',1
+
+select * from Mascotas	
+
+--hay que modficarlo de acuerdo a lo que se deberia mostrar cuando se busca una
+go
+alter procedure InfoMascota
+@CodMascota char(5)
+AS
+begin
+select Alias,Color_pelo,Raza,Tamaño,Apellido,Clientes.IdCliente,Especie from Mascotas
+inner join Clientes on Clientes.IdCliente = Mascotas.IdCliente
+where CodMascota = @CodMascota
+end
+
+
+
+
+GO
+
