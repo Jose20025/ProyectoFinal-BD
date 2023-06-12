4221417349b825542e6e330b86a4163300eb018b
Jose Carlos <joseca20025@gmail.com>
2023-06-12 18:10:13 -0400

    Eliminacion de ayuda.sql

--- sql/ayuda.sql
+++
@@ -1,8 +0,0 @@
-select * from Mascotas
-select * from HistorialesPeso
-select * from Estadias
-select * from Clientes
-select * from Estadias
-select * from Requerimientos
-
-select * from HistorialesMedicos
